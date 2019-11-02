0d2ca37e3ffc315db157162fa2c65d71c99e5468
jaimeleite <32790077+jaimeleite@users.noreply.github.com>
2019-11-02 22:58:29 +0000

    Update index.js

--- TP5/gestaoAlunos/routes/index.js
+++ TP5/gestaoAlunos/routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 var jsonfile = require('jsonfile')
 
-var myBD = __dirname + "/../data/alunos.json"
+var file = __dirname + "/../data/alunos.json"
 
 /* GET home page. */
 router.get('/alunos', function(req, res) {
-  jsonfile.readFile(myBD, (erro, dados) => {
+  jsonfile.readFile(file, (erro, dados) => {
     if(!erro){
         res.render('index', {listAlunos: dados})              
     }
@@ -17,7 +17,7 @@ router.get('/alunos', function(req, res) {
 })
 
 router.get('/registarAluno', function(req, res) {
-  jsonfile.readFile(myBD, (erro, dados) => {
+  jsonfile.readFile(file, (erro, dados) => {
     if(!erro){
         res.render('registoAluno', {listAlunos: dados})              
     }
@@ -30,7 +30,7 @@ router.get('/registarAluno', function(req, res) {
 router.get('/alunos/:idAluno', function(req, res) {
   var ident = req.params.idAluno
   console.log(ident)
-  jsonfile.readFile(myBD, (erro, alunos)=>{
+  jsonfile.readFile(file, (erro, alunos)=>{
     var indice = alunos.findIndex(c => c.identificador == ident)
       res.render('seeAluno', {al: alunos[indice]})
   })
@@ -40,7 +40,7 @@ router.get('/alunos/:idAluno', function(req, res) {
 router.get('/registoNotas/:id', function(req, res) {
   var id = req.params.id
   console.log(id)
-  jsonfile.readFile(myBD, (erro, dados) => {
+  jsonfile.readFile(file, (erro, dados) => {
     if(!erro){
         res.render('registaNotas', {idAluno: id})
     }
@@ -52,7 +52,7 @@ router.get('/registoNotas/:id', function(req, res) {
 
 router.post('/alunos/:idAluno/notas', function(req, res) {
   var ident = req.params.idAluno
-  jsonfile.readFile(myBD, (erro, alunos)=>{
+  jsonfile.readFile(file, (erro, alunos)=>{
       if(!erro){
         var indice = alunos.findIndex(c => c.identificador == ident)
         var alu = alunos[indice]
@@ -60,7 +60,7 @@ router.post('/alunos/:idAluno/notas', function(req, res) {
       
         alunos.splice(indice, 1)
         alunos.push(alu)
-        jsonfile.writeFile(myBD, alunos, erro => {
+        jsonfile.writeFile(file, alunos, erro => {
             if(erro) console.log(erro)
             else console.log('Registo gravado com sucesso.')
         })
@@ -71,7 +71,7 @@ router.post('/alunos/:idAluno/notas', function(req, res) {
 
 router.post('/alunos', function(req, res) {
     var al = req.body
-    jsonfile.readFile(myBD, (erro, alunos)=>{
+    jsonfile.readFile(file, (erro, alunos)=>{
         if(!erro){
           var i,controlo = 0
           for (i = 0; alunos[i]; i++) {
@@ -82,7 +82,7 @@ router.post('/alunos', function(req, res) {
           if(controlo == 0){
             al.notas=[]
             alunos.push(al)
-            jsonfile.writeFile(myBD, alunos, erro => {
+            jsonfile.writeFile(file, alunos, erro => {
                 if(erro) console.log(erro)
                 else console.log('Registo gravado com sucesso.')
             })
@@ -94,12 +94,12 @@ router.post('/alunos', function(req, res) {
 
 router.delete('/alunos/:idAluno', function(req, res) {
   var identificador = req.params.idAluno
-  jsonfile.readFile(myBD, (erro, alunos)=>{
+  jsonfile.readFile(file, (erro, alunos)=>{
     if(!erro){
       var posicao = alunos.findIndex(c => c.identificador == identificador)
       
       alunos.splice(posicao, 1)
-      jsonfile.writeFile(myBD, alunos, erro => {
+      jsonfile.writeFile(file, alunos, erro => {
         if(erro) console.log(erro)
         else console.log('Remoção do aluno efetuada com sucesso.')
       })
@@ -113,7 +113,7 @@ router.delete('/alunos/:idAluno/notas/:indicador', function(req, res) {
   var identAluno = req.params.idAluno
   var newAluno, posAluno
 
-  jsonfile.readFile(myBD, (erro, alunos)=>{
+  jsonfile.readFile(file, (erro, alunos)=>{
     if(!erro){
       posAluno = alunos.findIndex(c => c.identificador == identAluno)
           
@@ -127,7 +127,7 @@ router.delete('/alunos/:idAluno/notas/:indicador', function(req, res) {
           
       var indNewAluno = alunos.findIndex(c => c.identificador == newAluno.identificador)
 
-      jsonfile.writeFile(myBD, alunos, erro => {
+      jsonfile.writeFile(file, alunos, erro => {
         if(erro) console.log(erro)
         else{
           console.log('Nota removida com sucesso.')
